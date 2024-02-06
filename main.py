84239d9b4f5b44588d9e5400601fbb7da6ad5c36
lucas.zanona <lucasfariaszanona@gmail.com>
2024-02-06 15:56:26 -0300

    adding the withdraw function

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Q - Quit
 balance = 0.0
 extract = ""
+dailywd = 3;
 
 while True: 
     print('''Choose the desired option:
@@ -15,10 +16,22 @@ while True:
     option = input()
 
     if (option == 'W' or option == 'w'):
-        print("Withdraw")
+        withdraw = float(input("How much do you want to withdraw?"))
+
+        if dailywd < 1:
+            print("Daily withdrawal limit reached...")
+        elif withdraw > 500:
+            print("The withdraw has to be smaller than 500")
+        elif withdraw > balance:
+            print(f"The withdraw is bigger than the balance! The balance is {balance}")
+        else:
+            balance -= withdraw
+            print(f"Your current balance now is: {balance}")
+            extract = extract + f"Withdraw: {deposit}\n"
+            dailywd -= 1
 
     elif (option == 'D' or option == 'd'):
-        deposit = float(input("How do you want to deposit?"))
+        deposit = float(input("How much do you want to deposit?"))
 
         if deposit > 0:
             balance = balance + deposit
@@ -28,10 +41,11 @@ while True:
             print("Error, the value have to bigger than 0")
 
     elif (option == 'E' or option == 'e'):
+        print("Extract: ")
         print(extract)
-
-    elif (option == 'Q' or option == 'q'):
-        print("Quit")
         
+    elif (option == 'Q' or option == 'q'):
+        print("Quiting...")
+        break
     else: 
-        print("Invalid Option")
+        print("Invalid Option...")
